65035f3a60d0f44ec8de2b0354ca27407949c84b
Selithrarion <sergey.maltsev2004@yandex.ru>
2022-04-07 18:58:26 +0500

    audio: add welcome upload audio component

---
+++ frontend/src/components/audio/AudioWelcome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="column items-center w-full gap-4 q-pt-xl">
+    <div class="text-h3 text-weight-bold">Audio Tools</div>
+    <div class="text-subtitle2 q-mb-lg">Cut track, change volume, change bitrate online</div>
+
+    <CommonUploader accept="audio/*" @set-file="$emit('set-audio', $event)" @set-file-duration="$emit('set-audio-duration', $event)">
+      <BaseButton style="width: 250px" color="primary" label="Open file" size="18px" unelevated />
+    </CommonUploader>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CommonUploader from 'components/common/CommonUploader.vue';
+
+export default defineComponent({
+  name: 'AudioWelcome',
+
+  components: {
+    CommonUploader,
+  },
+
+  emits: ['set-audio', 'set-audio-duration'],
+
+  setup() {
+    return {};
+  },
+});
+</script>
